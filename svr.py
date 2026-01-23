1f9facbc4b04653cd6097d28df046f29d0e46ed8
Lauren Hendley <116697229+laurenhendley@users.noreply.github.com>
2026-01-23 11:04:43 -0500

    Add files via upload

--- svr.py
+++ svr.py
@@ -1,3 +1,23 @@
+""" LAST MEASURED ACCURACY
+MSE:  4528.179512947439
+MAE:  56.43424404556509
+R^2:  0.1611853727721696
+
+----
+
+MSE:  2825.4352622190386
+MAE:  41.856022295009396
+R^2:  0.476607228256293
+CV R2:  0.4739643004433741
+
+
+MSE:  3175.2840124100535
+MAE:  42.71147567093662
+R^2:  0.4118001135784157
+CV R2:  0.45896301212739055
+"""
+
+
 #### imports
 
 # Getting the dataset
@@ -9,6 +29,11 @@ from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, cross_val_score, learning_curve
+from sklearn.linear_model import Ridge
+
+import matplotlib.pyplot as mpl
+import numpy as np
 
 
 def model_exploring(ds):
@@ -26,16 +51,37 @@ def model_exploring(ds):
     print(ds.target)
 
 
-def model_generation(train_X, test_X, train_Y, test_Y):
-    model = Pipeline([
+def model_generation(train_X, test_X, train_Y):
+    param_grid = {
+        "svr__C": np.logspace(-1,4,100),
+        "svr__gamma": np.logspace(-4,1,10),
+        "svr__epsilon": [0.01, 0.1, 0.5, 1.0]
+    }
+
+    pipline = Pipeline([
         ("scaler", StandardScaler()),
         ("svr", SVR(kernel = "rbf"))
     ])
 
-    model.fit(train_X, train_Y)
-    y_pred = model.predict(test_X)
+    model = GridSearchCV(
+        pipline,
+        param_grid,
+        scoring="r2",
+        cv=5,
+        n_jobs=-1,
+        refit=True
+    )
 
-    return y_pred, model
+    train_y_log = np.log1p(train_Y)
+
+    model.fit(train_X, train_y_log)
+
+    best_model = model.best_estimator_
+
+    y_pred_log = best_model.predict(test_X)
+    y_pred = np.expm1(y_pred_log)
+
+    return y_pred, best_model
 
 def accuracy(y_test, y_pred):
     print("MSE: ", mean_squared_error(y_test, y_pred))
@@ -43,13 +89,53 @@ def accuracy(y_test, y_pred):
     print("R^2: ", r2_score(y_test, y_pred))
 
 
+def plot_metrics(model, ds):
+    train_size, train_score, val_score = learning_curve (
+        model,
+        ds.data,
+        ds.target,
+        cv = 5,
+        scoring="r2",
+        train_sizes=np.linspace(0.1,1.0,10)
+    )
+
+    train_mean = train_score.mean(axis=1)
+    val_mean = val_score.mean(axis = 1)
+
+    mpl.figure()
+    mpl.plot(train_size,train_mean,label="Training R2")
+    mpl.plot(train_size,val_mean,label="Validation R2")
+    mpl.xlabel("Training Samples")
+    mpl.ylabel("R2 score")
+    mpl.title("Diabetes DS SVR LC")
+    mpl.legend()
+    mpl.show()
+
+
 if __name__ == "__main__":
     ds = datasets.load_diabetes()
 
     train_X, test_X, train_Y, test_Y = train_test_split(ds.data, ds.target, test_size = 0.3, random_state = 42)
 
-    Y_pred, model = model_generation(train_X, test_X, train_Y, test_Y)
+    Y_pred, model = model_generation(train_X, test_X, train_Y)
 
     accuracy(test_Y, Y_pred)
+
+    ridge = Pipeline([
+        ("scaler", StandardScaler()),
+        ("ridge", Ridge(alpha=1.0))
+    ])
+
+    scores = cross_val_score(
+        model,
+        ds.data,
+        ds.target,
+        cv = 10,
+        scoring ="r2"
+    )
+
+    print("CV R2: ", scores.mean())
+
+    plot_metrics(model, ds)
 
 
